a1b155091f155f5476298ab919d706e7e7644d0f
Ming13124 <tanjunming23@gmail.com>
2022-04-20 09:32:06 +0800

    Place Order Feature

--- module3/src/modules/order-confirm/ConfirmOrderPage.js
+++ module3/src/modules/order-confirm/ConfirmOrderPage.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { LoginContext } from '../../contexts/LoginContext';
+import { Link } from 'react-router-dom';
 
 function ConfirmOrder() {
 
+    const { isLoggedIn } = useContext(LoginContext);
+
 
     return (
-        <div className="container mt-5" style={{textAlign: "center",justifyContent: "center"}}>
-            <h5 style={{color:"green"}}>
-            <i className="fas fa-check-circle"></i>&nbsp;
-                Congratulations. You have successfully redeemed your points.<br></br>
-                You will receive your vouchers in your registered mail id.
-    
-            </h5>
-        </div>
 
+        isLoggedIn ?
+            (
+                <div className="container mt-5" style={{ textAlign: "center", justifyContent: "center" }}>
+                    <h5 style={{ color: "green" }}>
+                        <i className="fas fa-check-circle"></i>&nbsp;
+                        Congratulations. You have successfully redeemed your points.<br></br>
+                        You will receive your vouchers in your registered mail id.
+
+                    </h5>
+                    <br></br>
+                    <h6 style={{ color: "green" }}>Redirecting to homepage...</h6>
+                </div>
+            ) :
+            (
+                <div className="mt-5">
+                    Please Sign in to redeem your points. &nbsp; &nbsp;
+                    <Link className="btn btn-primary" to="/login">
+                        <i className="fas fa-user" />&nbsp; Sign in
+
+                    </Link>
+                </div>
+            )
     )
 }
 export default ConfirmOrder
