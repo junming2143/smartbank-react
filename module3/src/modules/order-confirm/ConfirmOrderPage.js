30a8fbccc6f33e5ed7fb207e480e4494eae83cf3
Ming13124 <tanjunming23@gmail.com>
2022-04-23 16:42:37 +0800

    Solve persistence state

--- module3/src/modules/order-confirm/ConfirmOrderPage.js
+++ module3/src/modules/order-confirm/ConfirmOrderPage.js
@@ -1,36 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { LoginContext } from '../../contexts/LoginContext';
 import { Link } from 'react-router-dom';
+import { useState } from 'react/cjs/react.production.min';
+import { useHistory } from 'react-router-dom';
 
 function ConfirmOrder() {
 
-    const { isLoggedIn } = useContext(LoginContext);
-
+    let history = useHistory();
+    useEffect(() => {
+        if (sessionStorage.getItem("OrderConfirm") === null){
+            history.push("/")
+        }
+    }, [])
 
     return (
 
-        isLoggedIn ?
-            (
-                <div className="container mt-5" style={{ textAlign: "center", justifyContent: "center" }}>
-                    <h5 style={{ color: "green" }}>
-                        <i className="fas fa-check-circle"></i>&nbsp;
-                        Congratulations. You have successfully redeemed your points.<br></br>
-                        You will receive your vouchers in your registered mail id.
+            <div className="container mt-5" style={{ textAlign: "center", justifyContent: "center" }}>
+                <h5 style={{ color: "green" }}>
+                    <i className="fas fa-check-circle"></i>&nbsp;
+                    Congratulations. You have successfully redeemed your points.<br></br>
+                    You will receive your vouchers in your registered mail id.
 
-                    </h5>
-                    <br></br>
-                    <h6 style={{ color: "green" }}>Redirecting to homepage...</h6>
-                </div>
-            ) :
-            (
-                <div className="mt-5">
-                    Please Sign in to redeem your points. &nbsp; &nbsp;
-                    <Link className="btn btn-primary" to="/login">
-                        <i className="fas fa-user" />&nbsp; Sign in
-
-                    </Link>
-                </div>
-            )
+                </h5>
+                <br></br>
+                <h6 style={{ color: "green" }}>Redirecting to homepage...</h6>
+            </div>
+        
     )
 }
 export default ConfirmOrder
