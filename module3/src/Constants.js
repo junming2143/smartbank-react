2072cb623534d686ff48ae5db8493563a16f1e6c
junming2143 <tanjunming93@gmail.com>
2022-05-11 18:42:02 +0800

    A

--- module3/src/Constants.js
+++ module3/src/Constants.js
@@ -1,2 +1,5 @@
 
-export const API_URL = "https://reksu22-app2.brainupgrade.net/sm/";
+//export const API_URL = "https://reksu22-app2.brainupgrade.net/sm/";
+//export const EMAIL_URL = "https://reksu22-app2.brainupgrade.net/email/";
+export const API_URL = "http://localhost:8081/sm/";
+export const EMAIL_URL = "http://localhost:8083/email/";
